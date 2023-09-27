3c8783f7a61f8cb6714c93ec6e3d978fd4aa081a
root <root@Adam>
2023-09-27 11:11:45 +0200

    ajout de character

--- base_game/character.ts
+++ base_game/character.ts
@@ -1,4 +1,27 @@
-import Int_players from "./interface/Int_players"
-export default function character(path: string) {
+import Player from "./interface/Int_players";
+import Enemy from "./interface/Int_enemies";
+import Boss from "./interface/Int_bosses";
+import { stat } from "fs";
+export default function character(player: string) {
+    const fs = require('fs');
+    const content = fs.readFileSync(character, 'utf-8');
+    const stats.id = JSON.parse(content);
+
+
+
+//boucle fight
+    while (stats.hp > 0) {
+//fight enemy
+        if () {
+        stats.hp = stats.hp - stats.str;
+        } 
+//fight boss
+        else if () {
+        stats.hp = stats.hp - stats.str;
+        }
+    }
+
+
 
 }
+character(process.argv[2]);
