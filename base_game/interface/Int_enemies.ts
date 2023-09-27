efea73cea6e54caf50f51419efb4eaec6d434c5d
Lefevre Francois <lefevr_b@etna-alternance.net>
2023-09-27 10:41:39 +0200

    push interface

---
+++ base_game/interface/Int_enemies.ts
@@ -0,0 +1,16 @@
+export default interface Enemy {
+    id: number,
+    name: string,
+    hp: number,
+    mp: number,
+    str: number,
+    int: number,
+    def: number,
+    res: number,
+    spd: number,
+    luck: number,
+    race: number,
+    class: number,
+    rarity: number,
+
+}
