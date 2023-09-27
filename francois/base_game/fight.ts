a82ea18184503d2339a1609f1d810f0d485dc922
Lefevre Francois <lefevr_b@etna-alternance.net>
2023-09-27 14:59:56 +0200

    push

--- francois/base_game/fight.ts
+++ francois/base_game/fight.ts
@@ -1,23 +1,8 @@
-import Player from "./interface/Int_players";
-import Enemy from "./interface/Int_enemies";
-import Boss from "./interface/Int_bosses";
-import character from "./character";
-export default function fight(path: string) {
-   
-   
-   
-    //boucle fight
-    while (stats.hp > 0) {
-        //fight enemy
-                if () {
-                character.hp = character.hp - .str;
-                } 
-        //fight boss
-                else if () {
-                stats.hp = stats.hp - stats.str;
-                }
-            }
-        
-        
-        
-        }
+import enemy from './enemy'
+
+const enemychoisi = enemy("./../fichiers_json/enemies.json")
+
+const enemystats = [ enemychoisi.name, enemychoisi.hp, enemychoisi.str ]
+
+
+console.log("l'enemy", enemystats[0],"a", enemystats[1], "hp" )
