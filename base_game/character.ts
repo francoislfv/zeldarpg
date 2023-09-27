73a23dfeae9252f09d9e07f100da3e02f140a155
root <root@Adam>
2023-09-27 11:22:55 +0200

    ajout du fichier fight

--- base_game/character.ts
+++ base_game/character.ts
@@ -2,26 +2,11 @@ import Player from "./interface/Int_players";
 import Enemy from "./interface/Int_enemies";
 import Boss from "./interface/Int_bosses";
 import { stat } from "fs";
-export default function character(player: string) {
+export default function character(path: string) {
     const fs = require('fs');
-    const content = fs.readFileSync(character, 'utf-8');
-    const stats.id = JSON.parse(content);
-
-
-
-//boucle fight
-    while (stats.hp > 0) {
-//fight enemy
-        if () {
-        stats.hp = stats.hp - stats.str;
-        } 
-//fight boss
-        else if () {
-        stats.hp = stats.hp - stats.str;
-        }
-    }
+    const content = fs.readFileSync(path, 'utf-8');
+    const stats = JSON.parse(content);
 
 
 
 }
-character(process.argv[2]);
---
+++ base_game/fight.ts
@@ -0,0 +1,23 @@
+import Player from "./interface/Int_players";
+import Enemy from "./interface/Int_enemies";
+import Boss from "./interface/Int_bosses";
+import character from "./character";
+export default function fight(path: string) {
+   
+   
+   
+    //boucle fight
+    while (stats.hp > 0) {
+        //fight enemy
+                if () {
+                character.hp = character.hp - .str;
+                } 
+        //fight boss
+                else if () {
+                stats.hp = stats.hp - stats.str;
+                }
+            }
+        
+        
+        
+        }
