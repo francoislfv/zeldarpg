5595c55c48840ecbcc57acda5c04b9424993ab8c
Lefevre Francois <lefevr_b@etna-alternance.net>
2023-09-28 11:27:47 +0200

    push3 fight

--- francois/base_game/fight.ts
+++ francois/base_game/fight.ts
@@ -2,40 +2,54 @@ import * as rl from 'readline-sync';
 import enemy from './enemy';
 import character from './character';
 
-const enemyChoisi = enemy("./../fichiers_json/enemies.json");
-const characterChoisi = character("./../fichiers_json/players.json");
+let enemyChoisi = enemy("./../fichiers_json/enemies.json");
+let characterChoisi = character("./../fichiers_json/players.json");
 
 const enemyHpDebut: number = enemyChoisi.hp;
-let enemyMissingHp: number = enemyHpDebut - enemyChoisi.hp;
+let enemyFullHp: number = enemyChoisi.hp
+let enemyMissingHp: number = 0;
 
 const charHpDebut: number = characterChoisi.hp;
-const charMissingHp: number = charHpDebut - characterChoisi.hp;
+let characterFullHp: number = characterChoisi.hp;
+let charMissingHp: number = 0;
 
 export function inputMenu(): string {
 
-    const getInput = (question: string) => rl.question(`${question}\n`);
+    let getInput = (question: string) => rl.question(`${question}\n`);
     let action: string = getInput(`----- OPTIONS -----\n1. 🗡️  ATTACK      2. 🍎 HEAL`);
     return action;
+
 }
 
 export function hpDisplay(): void {
-    const underscore: string = "💔";
+
 
     console.log(` ⚔️  - FIGHT 1 - ⚔️\n`);
-    console.log(enemyMissingHp);
-    console.log(`\x1b[31m${enemyChoisi.name}\x1b[0m\nHP : ${"❤️ ".repeat(enemyChoisi.hp - enemyMissingHp)}${"💔".repeat(enemyMissingHp)}  ${enemyChoisi.hp}/${enemyHpDebut}\n`);
-    console.log(`\x1b[32m${characterChoisi.name}\x1b[0m\nHP : ${"❤️ ".repeat(characterChoisi.hp)}${underscore.repeat(charMissingHp)}  ${characterChoisi.hp}/${charHpDebut}\n`);
+    console.log(`\x1b[31m${enemyChoisi.name}\x1b[0m\nHP : ${"❤️ ".repeat(Math.max(0, enemyFullHp))}${"💔".repeat(enemyMissingHp)}  ${Math.max(0, enemyFullHp)}/${enemyHpDebut}\n`);
+    console.log(`\x1b[32m${characterChoisi.name}\x1b[0m\nHP : ${"❤️ ".repeat(Math.max(0, characterFullHp))}${"💔".repeat(charMissingHp)}  ${Math.max(0, characterFullHp)}/${charHpDebut}\n`);
+
 }
 
 export function fight(): void {
-    // const input = inputMenu();
+    const action = inputMenu();
 
-    if (inputMenu() === "1") {
-        console.log(`\x1b[3m You attacked and dealt ${characterChoisi.str} damages! \x1b[0m\n`);
-        console.log(`\x1b[3m ${enemyChoisi.name} attacked and dealt ${enemyChoisi.str} damages! \x1b[0m`);
-        enemyMissingHp = enemyChoisi.hp - characterChoisi.str;
-        console.log(enemyMissingHp);
-    } else if (inputMenu() === "2") {
+    if (action === "1") {
+        console.log(`\n\x1b[3m You attacked and dealt ${characterChoisi.str} damages! \x1b[0m\n`);
+        console.log(`\x1b[3m ${enemyChoisi.name} attacked and dealt ${enemyChoisi.str} damages! \x1b[0m\n`);
+
+
+        const damageDealtChar = Math.min(characterChoisi.str);
+        enemyMissingHp += damageDealtChar
+        enemyFullHp -= damageDealtChar
+
+        const damageDealtEnemy = Math.min(enemyChoisi.str)
+        charMissingHp += damageDealtEnemy
+        characterFullHp -= damageDealtEnemy
+
+
+
+
+    } else if (action === "2") {
         if (characterChoisi.hp < 60) {
             console.log(`\x1b[3m You used heal! \x1b[0m\n`);
             characterChoisi.hp += 10;
@@ -44,12 +58,20 @@ export function fight(): void {
         }
     }
 
-    if (enemyMissingHp <= 0) {
+   /* if (enemyFullHp === 0) {
         console.log(`\x1b[3m You won the fight! \x1b[0m\n`);
         return;
-    } else if (characterChoisi.hp <= 0) {
+    } else if (characterFullHp === 0) {
         console.log(`\x1b[3m You lost the fight! \x1b[0m\n`);
         return;
-    }
+    } */
 }
 
+hpDisplay()
+fight()
+hpDisplay()
+fight()
+hpDisplay()
+fight()
+hpDisplay()
+fight()
