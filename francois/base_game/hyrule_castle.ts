aed21157170a429cd46ad9fbf98c2196e402b9d7
Lefevre Francois <lefevr_b@etna-alternance.net>
2023-09-28 16:11:31 +0200

    push all

--- francois/base_game/hyrule_castle.ts
+++ francois/base_game/hyrule_castle.ts
@@ -1,10 +1,35 @@
 
-import { pressKeyToContinue, inputMenu, hpDisplay, fight,  } from './fight';
+import { pressKeyToContinue, inputMenu, hpDisplay, fight} from './fight';
+import  {Menu_welcome_player, Menu_difficulty, Menu_Rounds} from './menu';
 import * as rl from 'readline-sync';
 
-export function inputdebut(): string {
+
+
+let action1: string;
+let action2: string;
+let action3: string;
+
+export function input1(): string {
 
     let getInput = (question: string) => rl.question(`${question}\n`);
     let action1: string = getInput('');
     return action1;
+}
+
+export function input2(): string {
+
+    let getInput = (question: string) => rl.question(`${question}\n`);
+    let action2: string = getInput('');
+    return action2;
+}
+
+export function input3(): string {
+
+    let getInput = (question: string) => rl.question(`${question}\n`);
+    let action3: string = getInput('');
+    return action3;
+}
+
+if (action1 === "1") {
+    
 }
